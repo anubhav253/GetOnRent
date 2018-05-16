794c1d249e3799750916c7456902807a25f3b3a7
Anubhav Kumar <anubhav253kumar@gmail.com>
2018-05-16 11:01:31 +0530

    Aboutus and contactus page are implemented.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, session, logging, json, jsonify
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, TextField, SubmitField
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flask_session import Session
@@ -30,21 +30,6 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    phone = StringField('Mobile Number', [validators.Length(min=10, max=10)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-
 @app.route('/products')
 def products():
 
@@ -67,6 +52,43 @@ def product(id):
     result = cur.execute("SELECT * FROM bikes where id = %s", [id])
     product = cur.fetchone()
     return render_template('product.html', product=product)
+
+class ContactForm(Form):
+    name = TextField("Name")
+    email = TextField("Email")
+    subject = TextField("Subject")
+    message = TextAreaField("Message")
+    submit = SubmitField("Send")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contactus(name, email, subject, message) VALUES(%s, %s, %s, %s)", (name, email, subject, message))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Your message has been delivered.', 'success')
+        return redirect(url_for('index'))
+    return render_template('contact.html', form=form)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    phone = StringField('Mobile Number', [validators.Length(min=10, max=10)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
